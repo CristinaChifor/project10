823976d4d0d3d850bf1d2d51c8bc79efad9d00be
Cristina.Chifor <cristinachifor93.cc@gmail.com>
2024-03-04 20:53:02 +0200

    Add exercices

---
+++ script.js
@@ -0,0 +1,18 @@
+// exercitiul 1 -varsta
+
+let age = 30;
+console.log (`Varsta mea este ${age}`);
+console.log (age);
+
+// exercitiul 2- suma a doua numere.
+let numberA= 5;
+let numberB = 8;
+let sum = numberA + numberB;
+console.log (sum);
+console.log (`Rezultatul sumei dintre ${numberA} plus ${numberB} este ${sum}`);
+
+// exercitiul 3 conversie din C in Fh
+
+let gradeC = 10;
+let gradeF= (gradeC * 9/5) +32;
+console.log (`Conversia dintre C si Fh este egala cu ${gradeF}`);
